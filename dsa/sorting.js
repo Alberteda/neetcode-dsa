05d843ba4e8ca13418c399bd0fee314ad36a034c
alberteda <alberteda32@gmail.com>
2025-05-28 20:01:10 -0400

    add insertion sort

--- dsa/sorting.js
+++ dsa/sorting.js
@@ -1,1 +1,22 @@
 // Insertion Sort
+
+function insertionSort(arr) {
+  // i -> this is our current pointer (primarily used to traverse the data)
+  // j -> this is our previous pointer that is to the left of i by one position
+  for (let i = 1; i < arr.length; i++) {
+    let j = i - 1;
+
+    while (j >= 0 && arr[j + 1] < arr[j]) {
+      let temp = arr[j + 1];
+      arr[j + 1] = arr[j];
+      arr[j] = temp;
+      j--;
+    }
+  }
+
+  return arr;
+}
+
+// TESTING
+
+console.log(insertionSort([2, 8, 4, 1, 6]));
